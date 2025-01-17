091fac293a0be07e32110c6676e006c93b0f568a
Eugene Batalov <eugene@tensorlake.ai>
2025-01-17 12:31:06 +0000

    Add function process crash test
    This is an important test that validate that system progresses
    when a Function Executor that runs a function crashes for any reason.

---
+++ tests/src/test_function_process_crash.py
@@ -0,0 +1,44 @@
+import os
+import signal
+import unittest
+
+from testing import test_graph_name
+
+from tensorlake import Graph, RemoteGraph, tensorlake_function
+
+
+@tensorlake_function()
+def function(crash: bool) -> str:
+    if crash:
+        os.kill(os.getpid(), signal.SIGKILL)
+    return "success"
+
+
+class TestFunctionProcessCrash(unittest.TestCase):
+    def test_function_invoke_successful_after_process_crashes(self):
+        graph = Graph(
+            name=test_graph_name(self),
+            description="test",
+            start_node=function,
+        )
+        graph = RemoteGraph.deploy(graph)
+
+        print("Running a function that will crash FunctionExecutor process...")
+        for i in range(10):
+            crash_invocation_id = graph.run(
+                block_until_done=True,
+                crash=True,
+            )
+            crash_output = graph.output(crash_invocation_id, "function")
+            self.assertEqual(crash_output, [])
+
+        success_invocation_id = graph.run(
+            block_until_done=True,
+            crash=False,
+        )
+        success_output = graph.output(success_invocation_id, "function")
+        self.assertEqual(success_output, ["success"])
+
+
+if __name__ == "__main__":
+    unittest.main()
