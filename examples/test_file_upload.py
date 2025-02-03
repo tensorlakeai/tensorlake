bc38734bdd48b08bdc919740f5122b9484f02370
Diptanu Gon Choudhury <diptanuc@gmail.com>
2025-02-03 13:38:02 -0800

    update sdk

--- examples/test_file_upload.py
+++ examples/test_file_upload.py
@@ -1,5 +1,5 @@
 from tensorlake.data_loaders import LocalDirectoryLoader
-from tensorlake.documentai.file import upload_file_sync
+from tensorlake.documentai.file import upload_file_sync, upload_files_sync
 from tensorlake.documentai.parser import DocumentParser, ParsingOptions
 from itertools import batched
 import time
@@ -15,28 +15,23 @@ file_ids = {}
 
 job_ids = {}
 
-for files in batched(files, 10):
-    for file in files:
-        time.sleep(1)
-        file_id = upload_file_sync(file.path, api_key=api_key) 
-        print(f"uploading file {file.path} size {file.file_size/2048} mb")
-        print(file_id)
-        file_ids[file.name] = file_id
-
-for file_name, file_id in file_ids.items():
-    job_id = document_parser.parse_document(file_id, ParsingOptions())
-    print(job_id)
-    job_ids[job_id] = (file_name, file_id)
-
-import csv
-csv_file = "job_ids.csv"
-with open(csv_file, mode='w') as file:
-    writer = csv.writer(file)
-    writer.writerow(["job_id", "file_name", "file_id"])
-    for job_id, (file_name, file_id) in job_ids:
-        writer.writerow([job_id, file_name, file_id])
+file_ids = upload_files_sync(files, api_key=api_key) 
 print(file_ids)
-print(job_ids)
+
+#for file_name, file_id in file_ids.items():
+#    job_id = document_parser.parse_document(file_id, ParsingOptions())
+#    print(job_id)
+#    job_ids[job_id] = (file_name, file_id)
+#
+#import csv
+#csv_file = "job_ids.csv"
+#with open(csv_file, mode='w') as file:
+#    writer = csv.writer(file)
+#    writer.writerow(["job_id", "file_name", "file_id"])
+#    for job_id, (file_name, file_id) in job_ids.items():
+#        writer.writerow([job_id, file_name, file_id])
+#print(file_ids)
+#print(job_ids)
 
 
 
--- src/tensorlake/documentai/file.py
+++ src/tensorlake/documentai/file.py
@@ -1,6 +1,6 @@
 import httpx
 import requests
-from typing import Union, Dict
+from typing import Union, Dict, List
 from pathlib import Path
 from retry import retry
 from httpx import ConnectTimeout
@@ -48,8 +48,58 @@ def upload_file_sync(
             )
             response.raise_for_status()
             resp = response.json()
+            print(resp)
             return resp.get("id")
+        
+retry(tries=10, delay=2)
+def upload_files_sync(
+    file_paths: List[Union[str, Path]],
+    api_key: str = "",
+    base_url: str = "https://api.tensorlake.ai"
+) -> str:
+    """
+    Synchronously upload a file to the Tensorlake
+    
+    Args:
+        file_path: Path to the file to upload
+        api_key: API authentication token
+        base_url: Base URL for the API (optional)
+        
+    Returns:
+        Dict containing the API response
+        
+    Raises:
+        httpx.HTTPError: If the request fails
+        FileNotFoundError: If the file doesn't exist
+    """
+    #file_path = Path(file_path)
+    #if not file_path.exists():
+    #    raise FileNotFoundError(f"File not found: {file_path}")
+        
+    headers = {
+        "Authorization": f"Bearer {api_key}"
+    }
+    
+    url = f"{base_url}/documents/v1/files"
+    file_ids = []
+    with httpx.Client(headers=headers, base_url="https://api.tensorlake.ai") as client:
+        for file_path in file_paths:
+            file_path = Path(file_path.path)
+            with open(file_path, "rb") as f:
+                files = {"file": (file_path.name, f)}
+                response = client.post(
+                    url="/documents/v1/files",
+                    files=files,
+                    timeout=None,
+                )
+                response.raise_for_status()
+                resp = response.json()
+                print(resp)
+                file_id = resp.get("id")
+                file_ids.append(file_id)
+    return file_ids
 
+    
 async def upload_file_async(
     file_path: Union[str, Path],
     api_key: str = "",
