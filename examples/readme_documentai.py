71ff4438a93916ba998208e76429ddbc549c929d
Diptanu Gon Choudhury <diptanuc@gmail.com>
2025-02-03 23:54:35 -0800

    update readme_documentai.py

--- examples/readme_documentai.py
+++ examples/readme_documentai.py
@@ -2,28 +2,25 @@ import time
 from tensorlake.documentai import DocumentParser, Files, ParsingOptions, Jobs
 
 
-API_KEY = "tl_apiKey_CTmmqmqgQhwLtgHGwffWz_iNPyz1PRW5n9n3lpI-XPxEEjZq_WZj"
+API_KEY = "tl_XXXXX"
 
-# files = Files(api_key=API_KEY)
-# file_id = files.upload(path="./examples/appliance-repair-invoice-2.pdf")
+files = Files(api_key=API_KEY)
+file_id = files.upload(path="./examples/appliance-repair-invoice-2.pdf")
 
-# parser = DocumentParser(api_key=API_KEY)
-# job_id = parser.parse(file_id, options=ParsingOptions())
-# print(job_id)
-
-job_id="job-gP7LthGQNB8d9B6GgJLK9"
+parser = DocumentParser(api_key=API_KEY)
+job_id = parser.parse(file_id, options=ParsingOptions())
 
 jobs_client = Jobs(api_key=API_KEY)
 
 result = jobs_client.get(job_id=job_id)
-print(result)
+print(f"job status: {result.status}")
 while True:
     if result.status == "processing":
         print("waiting 5s...")
         time.sleep(5)
         result = jobs_client.get(job_id)
+        print(f"job status: {result.status}")
     else:
-        print(result.status)
         if result.status == "successful":
             print(result)
         break
