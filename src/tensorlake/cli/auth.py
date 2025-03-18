7e591eb9cd41e6412f4419452f783e92a07fdfc2
Benjamin Boudreau <boudreau.benjamin@gmail.com>
2025-03-18 12:07:11 -0400

    feat: enable json output on auth status to support getting projectId

--- src/tensorlake/cli/auth.py
+++ src/tensorlake/cli/auth.py
@@ -1,3 +1,4 @@
+import json
 import click
 
 from tensorlake.cli._common import AuthContext, with_auth
@@ -12,8 +13,26 @@ def auth():
 
 
 @auth.command(help="Print authentication status")
+@click.option(
+    "--output",
+    "-o",
+    type=click.Choice(["text", "json"]),
+    default="text",
+    help="Output format",
+)
 @with_auth
-def status(auth: AuthContext):
+def status(auth: AuthContext, output: str):
+    if output == "json":
+        print(
+            json.dumps(
+                {
+                    "organizationId": auth.organization_id,
+                    "projectId": auth.project_id,
+                    "apiKeyId": auth.api_key_id,
+                }
+            )
+        )
+        return
     click.echo(f"Organization ID: {auth.organization_id}")
     click.echo(f"Project ID     : {auth.project_id}")
     click.echo(f"API Key ID     : {auth.api_key_id}")
