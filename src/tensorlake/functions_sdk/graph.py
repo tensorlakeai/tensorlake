8a0c2c7a862e61f9b1a637f3d01ad2df98b33d64
Diptanu Gon Choudhury <diptanuc@gmail.com>
2025-03-22 23:24:30 -0700

    removing rich

--- src/tensorlake/functions_sdk/graph.py
+++ src/tensorlake/functions_sdk/graph.py
@@ -20,7 +20,6 @@ import cloudpickle
 import nanoid
 from nanoid import generate
 from pydantic import BaseModel
-from rich import print  # TODO: Migrate to use click.echo
 from typing_extensions import get_args, get_origin
 
 from .data_objects import Metrics, RouterOutput, TensorlakeData
