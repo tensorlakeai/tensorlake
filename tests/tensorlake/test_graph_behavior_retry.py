6bf47a382af22417449ae69f9f3d7ff82d291e2c
Diptanu Gon Choudhury <diptanuc@gmail.com>
2025-06-15 00:02:34 -0700

    update http client to use allocation id while fetching logs

---
+++ tests/tensorlake/test_graph_behavior_retry.py
@@ -0,0 +1,36 @@
+import unittest
+import testing
+from pydantic import BaseModel
+import parameterized
+
+from tensorlake.functions_sdk.graph import Graph
+from testing import remote_or_local_graph, test_graph_name
+from tensorlake import tensorlake_function
+from tensorlake.functions_sdk.retries import Retries
+
+
+class MyObject(BaseModel):
+    x: str
+
+@tensorlake_function(retries=Retries(max_retries=4))
+def simple_function(x: MyObject) -> MyObject:
+    if x.x == "a":
+        raise Exception("test exception")
+    print("simple_function", x.x)
+    return MyObject(x=x.x + "b")
+
+
+
+class TestGraphBehaviorRetry(unittest.TestCase):
+    def test_simple_function(self):
+        graph = Graph(
+            name=test_graph_name(self), description="test", start_node=simple_function
+        )
+        graph = remote_or_local_graph(graph, remote=True)
+        invocation_id = graph.run(block_until_done=True, x=MyObject(x="a"))
+        output = graph.output(invocation_id, "simple_function")
+        self.assertTrue(len(output) == 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
