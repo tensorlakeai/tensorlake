d58cdffecbc413e11a42bc86903d271cc4b3e4b0
Eugene Batalov <eugene@tensorlake.ai>
2025-07-08 12:48:32 +0100

    Prepare test runner for multiple test suites
    * Rename tests/tensorlake dir to tests/workflows_sdk
    * Add a flag to only run FE test suite to the test runner script
    * Allow choosing test suites to run in test runner scripts
    
    All these allow to run FE tests more conveniently during FE
    development and also prepare us for a separate test suite for
    Doc AI SDK.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,19 +7,74 @@ if [[ -z "$INDEXIFY_URL" ]]; then
     exit 1
 fi
 
+enable_fe_test_suite=false
+enable_workflows_sdk_test_suite=false
+enable_cli_test_suite=false
+enable_utils_test_suite=false
+
+if [[ "$1" == "--function-executor" ]]; then
+  enable_fe_test_suite=true
+else
+  # All by default
+  enable_fe_test_suite=true
+  enable_workflows_sdk_test_suite=true
+  enable_cli_test_suite=true
+  enable_utils_test_suite=true
+fi
+
+tests_exit_code=0
+
+run_test_suite() {
+  local test_files=$1
+  local test_suite_name=$2
+  local test_suite_exit_code=0
+
+  # Run each test file one by one sequentially. Set $tests_exit_code to non zero
+  # value if any of the test commands return non zero status code. Don't
+  # stop if a test command fails.
+  for test_file in $test_files; do
+    echo "Running $test_file for $test_suite_name test suite"
+    poetry run python $test_file
+    local test_file_exit_code=$?
+    if [ $test_file_exit_code -ne 0 ]; then
+      echo "One or more tests failed in $test_file for $test_suite_name test suite." | tee -a $summary_file
+    fi
+    tests_exit_code=$((tests_exit_code || test_file_exit_code))
+  done
+}
+
 # cd to the script's directory.
 cd "$(dirname "$0")"
 
-# Run each test file one by one sequentially. Returns non zero status
-# code if any of the test commands return non zero status code. Doesn't
-# stop if a test command fails.
-find . -name 'test_*.py' | xargs -L1 poetry run python
-TESTS_EXIT_CODE=$?
+summary_file=".run_tests_summary.txt"
+rm -f $summary_file
 
-if [ $TESTS_EXIT_CODE -eq 0 ]; then
-  echo "All tests passed!"
-else
-  echo "One or more tests failed. Please check output log for details."
+function_executor_test_files=$(find ./function_executor -name 'test_*.py')
+workflows_sdk_test_files=$(find ./workflows_sdk -name 'test_*.py')
+cli_test_files=$(find ./cli -name 'test_*.py')
+utils_test_files=$(find ./utils -name 'test_*.py')
+
+if [ "$enable_fe_test_suite" = true ]; then
+  run_test_suite "$function_executor_test_files" "Function Executor"
 fi
 
-exit $TESTS_EXIT_CODE
+if [ "$enable_workflows_sdk_test_suite" = true ]; then
+  run_test_suite "$workflows_sdk_test_files" "Workflows SDK"
+fi
+
+if [ "$enable_cli_test_suite" = true ]; then
+  run_test_suite "$cli_test_files" "CLI"
+fi
+
+if [ "$enable_utils_test_suite" = true ]; then
+  run_test_suite "$utils_test_files" "Utils"
+fi
+
+if [ $tests_exit_code -eq 0 ]; then
+  echo "All tests passed!" >> $summary_file
+else
+  echo "One or more tests failed. Please check output log for details." >> $summary_file
+fi
+
+cat $summary_file
+exit $tests_exit_code
