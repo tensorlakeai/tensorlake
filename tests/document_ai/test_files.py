6e14812be208a91476f6ff55bf251acece39773a
Miguel Hernández <miguel@tensorlake.ai>
2025-07-29 17:34:58 +0200

    add test for files upload

---
+++ tests/document_ai/test_files.py
@@ -0,0 +1,38 @@
+import os
+import unittest
+
+from tensorlake.documentai import DocumentAI
+
+class TestFiles(unittest.TestCase):
+    def setUp(self):
+        server_url = os.getenv("INDEXIFY_URL")
+        self.assertIsNotNone(
+            server_url, "INDEXIFY_URL environment variable is not set."
+        )
+
+        api_key = os.getenv("TENSORLAKE_API_KEY")
+        self.assertIsNotNone(
+            api_key, "TENSORLAKE_API_KEY environment variable is not set."
+        )
+
+        self.doc_ai = DocumentAI(
+            server_url=server_url,
+            api_key=api_key,
+        )
+        self.addCleanup(self.doc_ai.close)
+
+    def test_uploads_small_file(self):
+        file_id = self.doc_ai.upload(
+            "./document_ai/testdata/example_bank_statement.pdf",
+        )
+        self.assertIsNotNone(file_id)
+
+    def test_uploads_larger_files_than_10_mb(self):
+        file_id = self.doc_ai.upload(
+            "./document_ai/testdata/synthetic_20MB.csv",
+        )
+        self.assertIsNotNone(file_id)
+
+
+if __name__ == '__main__':
+    unittest.main()
