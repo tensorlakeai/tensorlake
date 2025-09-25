03ddc3b2ac838bcd283bc9f35258e65bbbd05a6c
Eugene Batalov <eugene@tensorlake.ai>
2025-09-25 22:59:01 +0500

    Fix app code serialization test and delete workflows_sdk tests dir

---
+++ tests/workflows/app_code_serialization/test_app_code_serialization.py
@@ -0,0 +1,127 @@
+import unittest
+from typing import List
+
+# Import from local package using absolute import path.
+from hello_world.hello_world import hello_world
+from hello_world.subpackage.subpackage import (
+    TensorlakeFunctionSubpackageHelloWorld,
+    subpackage_hello_world,
+    tensorlake_function_subpackage_hello_world,
+)
+
+# Import local module using absolute import path, works because the the code dir zip is added to PYTHONPATH.
+from repeat import repeat_hello_world
+
+# Check that symlinks inside the code dir works.
+from test_complex_graph_symlink import test_graph_api_fan_in
+
+import tensorlake.workflows.interface as tensorlake
+from tensorlake.workflows.remote.deploy import deploy
+
+# Warning: this test file is not a part of Python package so relative import from it are not possible.
+# Some subdirs in this code directory are Python packages (they have __init__.py file) and some are not.
+# The Python modules (files) inside Python packages can do relative imports and we test it there.
+
+# Absolute imports work in this file because the code directory is added to sys.path both when running
+# the test file as a script using poetry and when running graph code zip in Function Executor.
+
+
+@tensorlake.api()
+@tensorlake.function()
+def call_hello_world(_: str) -> str:
+    return hello_world()
+
+
+@tensorlake.api()
+@tensorlake.function()
+def call_repeat_hello_world(times: int) -> List[str]:
+    return repeat_hello_world(times)
+
+
+@tensorlake.api()
+@tensorlake.function()
+def call_hello_world_from_subpackage(_: str) -> str:
+    return subpackage_hello_world()
+
+
+@tensorlake.api()
+@tensorlake.function()
+def function_from_symlink_is_available(_: str) -> bool:
+    return True if test_graph_api_fan_in else False
+
+
+@tensorlake.api()
+@tensorlake.function()
+def import_from_subdir_fails(_: str) -> bool:
+    try:
+        from hello_world.subdir.subdir import foo
+
+        return False
+    except ImportError:
+        return True
+
+
+class TestApplicationCodeSerialization(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        deploy(__file__)
+
+    def test_call_hello_world(self):
+        request: tensorlake.Request = tensorlake.call_remote_api(
+            call_hello_world, "test"
+        )
+        self.assertEqual(request.output(), hello_world())
+
+    def test_call_repeat_hello_world(self):
+        request: tensorlake.Request = tensorlake.call_remote_api(
+            call_repeat_hello_world, 3
+        )
+        self.assertEqual(request.output(), repeat_hello_world(3))
+
+    def test_call_hello_world_from_subpackage(self):
+        request: tensorlake.Request = tensorlake.call_remote_api(
+            call_hello_world_from_subpackage, "test"
+        )
+        self.assertEqual(request.output(), subpackage_hello_world())
+
+    def test_function_from_symlink_is_available(self):
+        request: tensorlake.Request = tensorlake.call_remote_api(
+            function_from_symlink_is_available, "test"
+        )
+        self.assertTrue(request.output())
+
+    def test_import_from_subdir_fails(self):
+        # Direct import from subdir works when this test file is directly executed by Python.
+        # This is some known peculiar behavior of Python import system.
+        # There's no clear explanation/reference on why it works this way.
+        from hello_world.subdir.subdir import foo
+
+        self.assertIsNotNone(foo)
+
+        # However when this file is imported as a module, the direct import fails.
+        # Test that this happens indeed.
+        request: tensorlake.Request = tensorlake.call_remote_api(
+            import_from_subdir_fails, "test"
+        )
+        self.assertTrue(request.output())
+
+    def test_imported_tensorlake_function(self):
+        # Check that the function imported from the module works.
+        request: tensorlake.Request = tensorlake.call_remote_api(
+            tensorlake_function_subpackage_hello_world, "test"
+        )
+        self.assertEqual(request.output(), subpackage_hello_world())
+
+    def test_imported_tensorlake_class(self):
+        # Check that the function imported from the module works.
+        request: tensorlake.Request = tensorlake.call_remote_api(
+            TensorlakeFunctionSubpackageHelloWorld().run, "test"
+        )
+        self.assertEqual(request.output(), subpackage_hello_world())
+
+
+# TODO: Add test case that validates that multiprocessing works.
+
+
+if __name__ == "__main__":
+    unittest.main()
