9e88cc80a661e4458eecf681844c84b3c4d58aef
Eugene Batalov <eugene@tensorlake.ai>
2025-09-26 17:03:40 +0500

    Use `from tensorlake import X` syntat in test_output_propagation.py
    Just to show a different style of imports in action.

--- tests/workflows/test_output_propagation.py
+++ tests/workflows/test_output_propagation.py
@@ -1,52 +1,59 @@
 import unittest
 
-import tensorlake.workflows.interface as tensorlake
+from tensorlake.workflows.interface import (
+    Request,
+    api,
+    call_api,
+    call_remote_api,
+    function,
+    reduce,
+)
 from tensorlake.workflows.remote.deploy import deploy
 
 
-@tensorlake.api(output_serializer="pickle")
-@tensorlake.function()
+@api(output_serializer="pickle")
+@function()
 def api_function_output_propagation(payload: str) -> str:
     return foo()
 
 
-@tensorlake.function()
+@function()
 def foo() -> str:
     return bar()
 
 
-@tensorlake.function()
+@function()
 def bar() -> str:
     return buzz()
 
 
-@tensorlake.function()
+@function()
 def buzz() -> str:
     return "buzz"
 
 
-@tensorlake.api(output_serializer="pickle")
-@tensorlake.function()
+@api(output_serializer="pickle")
+@function()
 def api_reducer_output_propagation(payload: str) -> str:
     return foo_reducer()
 
 
-@tensorlake.function()
+@function()
 def foo_reducer() -> str:
     return bar_reducer()
 
 
-@tensorlake.function()
+@function()
 def bar_reducer() -> str:
     return buzz_reducer()
 
 
-@tensorlake.function()
+@function()
 def buzz_reducer() -> str:
-    return tensorlake.reduce(concat_strings, "buzz_reducer")
+    return reduce(concat_strings, "buzz_reducer")
 
 
-@tensorlake.function()
+@function()
 def concat_strings(a: str, b: str) -> str:
     return a + b
 
@@ -54,17 +61,13 @@ def concat_strings(a: str, b: str) -> str:
 class TestOutputPropagation(unittest.TestCase):
     def test_function_output_propagation(self):
         deploy(__file__)
-        request: tensorlake.Request = tensorlake.call_remote_api(
-            api_function_output_propagation, "test"
-        )
+        request: Request = call_remote_api(api_function_output_propagation, "test")
         output: str = request.output()
         self.assertEqual(output, "buzz")
 
     def test_reducer_output_propagation(self):
         deploy(__file__)
-        request: tensorlake.Request = tensorlake.call_remote_api(
-            api_reducer_output_propagation, "test"
-        )
+        request: Request = call_remote_api(api_reducer_output_propagation, "test")
         output: str = request.output()
         self.assertEqual(output, "buzz_reducer")
 
