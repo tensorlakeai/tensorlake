b72b924f680abb5e223d40f13c319835b93e6d22
Diptanu Gon Choudhury <diptanuc@gmail.com>
2025-09-26 20:49:56 -0700

    update app update test

--- tests/workflows/app_update/update_code_v2.py
+++ tests/workflows/app_update/update_code_v2.py
@@ -1,6 +1,13 @@
 import tensorlake.workflows.interface as tensorlake
 
 
+# FIXME: Temporary use "pickle" serializer until root function call of the returned
+# call tree inherits its output serializer from the API function.
+@tensorlake.api(output_serializer="pickle")
+@tensorlake.function()
+def update_code_start_func(_sleep_sec: int) -> str:
+    return update_code_end_func()
+
 @tensorlake.function()
 def update_code_end_func() -> str:
     return "update_code_end_func_v2"
