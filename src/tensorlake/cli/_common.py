f75da7abd443741b488791f4282272828a8627eb
David Calavera <1050+calavera@users.noreply.github.com>
2025-10-01 13:03:46 -0700

    Add subcommand to fetch application logs from the telemetry service. (#359)
    This change adds two new subcommands under application and request to be
    able to fetch application logs for them.
    
    The application subcommand accepts additional filters besides the
    request, like the function and container where the function runs.
    
    Formats:
    
    I ended up going a little bit overboard with formatting, but I think it
    gives people flexibility. These are the, not 2, not 3, but 4!!!!! format
    options:
    
    **Compact formatting**
    
    This is the default format. It uses two lines to make it nicely to read
    in laptop screens as well as big screens.
    
    <img width="812" height="437" alt="image"
    src="https://github.com/user-attachments/assets/0423ab46-d0a7-49ad-b546-2c16ec2feb19"
    />
    
    **Expanded formatting**
    
    This is like the compact format, but it shows the `logAttributes` that a
    user, or us, might have added to the log.
    
    <img width="1349" height="979" alt="image"
    src="https://github.com/user-attachments/assets/39d20112-923d-4598-b8d5-f9e1936be17f"
    />
    
    **Long formatting**
    
    This prints everything in one line of long text.
    
    <img width="1709" height="936" alt="image"
    src="https://github.com/user-attachments/assets/f1bb550c-906b-4674-a7ff-b519811c48dd"
    />
    
    **JSON formatting**
    
    This prints each log entry as a pretty json object.
    
    <img width="1701" height="886" alt="image"
    src="https://github.com/user-attachments/assets/f5598ef9-99a4-4501-907e-8a82deb1b097"
    />

--- src/tensorlake/cli/_common.py
+++ src/tensorlake/cli/_common.py
@@ -1,9 +1,16 @@
 import importlib.metadata
+import json
+import os
 import sys
+from asyncio.unix_events import SelectorEventLoop
+from cmath import e
 from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
 
 import click
 import httpx
+from rich import print, print_json
 
 try:
     VERSION = importlib.metadata.version("tensorlake")
@@ -11,7 +18,7 @@ except importlib.metadata.PackageNotFoundError:
     VERSION = "unknown"
 
 
-from tensorlake.applications.remote.api_client import APIClient
+from tensorlake.applications.remote.api_client import APIClient, LogEntry, LogsPayload
 
 from .config import get_nested_value, load_config
 
@@ -115,3 +122,111 @@ class Context:
 
 """Pass the Context object to the click command"""
 pass_auth = click.make_pass_decorator(Context)
+
+
+START_LINE = "┏"
+LINE = "┃"
+END_LINE = "┗"
+
+
+class LogFormat(Enum):
+    COMPACT = "compact"
+    EXPANDED = "expanded"
+    LONG = "long"
+    JSON = "json"
+
+
+def print_application_logs(logs: LogsPayload, format: LogFormat):
+    if format == LogFormat.LONG:
+        print_text_logs(logs.logs)
+    elif format == LogFormat.JSON:
+        print_json_logs(logs.logs)
+    elif format == LogFormat.COMPACT:
+        print_pretty_logs(logs.logs)
+    elif format == LogFormat.EXPANDED:
+        print_pretty_logs(logs.logs, full=True)
+
+
+def print_text_logs(logs: list[LogEntry]):
+    if len(logs) == 0:
+        return
+
+    for log in logs:
+        print(format_log_entry(log))
+
+
+def print_json_logs(logs: list[LogEntry]):
+    if len(logs) == 0:
+        return
+
+    for line in logs:
+        print_json(line.model_dump_json(), sort_keys=True)
+
+
+def format_log_entry(log: LogEntry) -> str:
+    timestamp = format_timestamp(log.timestamp)
+    keys = [
+        "ai.tensorlake.function_name",
+        "ai.tensorlake.container.id",
+        "ai.tensorlake.request.id",
+    ]
+    attrs = {key: value for key, value in log.resource_attributes if key in keys}
+    return f"{timestamp} {log.body} {attrs} {log.log_attributes}"
+
+
+def print_pretty_logs(logs: list[LogEntry], full: bool = False):
+    if len(logs) == 0:
+        return
+
+    for line in logs:
+        sys.stdout.write(format_pretty_log_entry(line, full=full))
+
+
+def format_timestamp(timestamp: int) -> str:
+    return datetime.fromtimestamp(timestamp / 1_000_000_000).strftime(
+        "%Y-%m-%dT%H:%M:%S%z"
+    )
+
+
+def format_pretty_log_entry(log: LogEntry, full: bool = False) -> str:
+    """
+    Format a single LogEntry in a human-friendly, colorized style.
+    """
+    ts = format_timestamp(log.timestamp)
+
+    # extract common resource attributes
+    resource = dict(log.resource_attributes or [])
+    function_name = resource.get("ai.tensorlake.function_name")
+    container_id = resource.get("ai.tensorlake.container.id")
+    request_id = resource.get("ai.tensorlake.request.id")
+
+    source = f"at {log.application}/{function_name}"
+    if request_id:
+        source += f":{request_id}"
+    if container_id:
+        source += f" [{container_id}]"
+
+    attrs = (
+        json.dumps(json.loads(log.log_attributes), indent=2, sort_keys=True)
+        if full
+        else None
+    )
+
+    second_line_prefix = LINE if attrs else END_LINE
+    ts_dim = click.style(ts, dim=True)
+    src_dim = click.style(source, dim=True, italic=True)
+
+    message = f"{START_LINE} {log.body} {ts_dim}\n"
+    message += f"{second_line_prefix} {src_dim}\n"
+
+    if attrs:
+        lines = attrs.splitlines()
+        line_number = 1
+        for line in lines[:-1]:
+            line_dim = click.style(f"[{line_number:2d}] {line}", dim=True)
+            message += f"{LINE} {line_dim}\n"
+            line_number += 1
+        line_dim = click.style(f"[{line_number:2d}] {lines[-1]}", dim=True)
+        message += f"{END_LINE} {line_dim}\n"
+
+    return message
