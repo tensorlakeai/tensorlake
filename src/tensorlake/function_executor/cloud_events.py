a12955477d4b114046b1a605f2512870fa9a5b8d
David Calavera <1050+calavera@users.noreply.github.com>
2025-10-16 14:38:53 -0700

    Remove push_to_collector function from SDK.
    We're going to put it in the Indexify executor.

--- src/tensorlake/function_executor/cloud_events.py
+++ src/tensorlake/function_executor/cloud_events.py
@@ -30,41 +30,5 @@ def new_cloud_event(event: dict[str, Any]) -> dict[str, Any]:
     return event_dict
 
 
-class Resource(BaseModel):
-    namespace: str
-    application: str
-    application_version: str
-    executor_id: str
-    fn_executor_id: str
-    fn: str
-
-
-def push_cloud_event_to_collector(
-    resource: Resource,
-    event: dict[str, Any],
-    collector_url: str | None = None,
-) -> None:
-    """
-    Pushes the given event to a log collector.
-    This function relies on the existence of the TENSORLAKE_COLLECTOR_URL environment variable.
-    If the environment variable is not set, the event is ignored.
-
-    This function does not capture any exceptions that may occur during the HTTP request
-    because it's designed to be embedded into the executor.
-    The executor needs to handle HTTP errors and collect metrics.
-    """
-    collector_url = (
-        os.environ.get("TENSORLAKE_COLLECTOR_URL")
-        if collector_url is None
-        else collector_url
-    )
-    if collector_url:
-        body = resource.model_dump()
-        body["event"] = new_cloud_event(event)
-
-        response = httpx.post(collector_url, json=body)
-        response.raise_for_status()
-
-
 def current_time() -> str:
     return datetime.datetime.now(datetime.timezone.utc).isoformat() + "Z"
