3a19c0a2a1437579ca4664f5cd5456829802eecd
Shanshan Wang <shanshan@tensorlake.ai>
2025-10-30 22:37:30 -0500

    model06 description

--- src/tensorlake/documentai/models/_enums.py
+++ src/tensorlake/documentai/models/_enums.py
@@ -82,7 +82,7 @@ class OcrPipelineProvider(str, Enum):
     Tensorlake01: It's fast but could have lower accuracy on complex tables. It's good for legal documents with footnotes.
     Tensorlake02: It's slower but could have higher accuracy on complex tables. It's good for financial documents with merged cells.
     Tensorlake03: A compact model that we deliver to on-premise users. It takes about 2 minutes to startup on Tensorlake's Cloud because it's meant for testing for users who are eventually going to deploy this model on dedicated hardware in their own datacenter.
-    Tensorlake06: It is the fastest model we have, but it is not the most accurate. It is good for plain text or standard documents.
+    Tensorlake06: Our fastest model, offering the best balance between speed and accuracy. It performs well on plain text or standard documents without complex layouts.
     """
 
     TENSORLAKE01 = "model01"
