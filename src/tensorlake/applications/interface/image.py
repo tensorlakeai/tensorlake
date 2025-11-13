829dde0788143293b87dc59fe1e9e14bd06675ac
Miguel Hernández <miguel@tensorlake.ai>
2025-11-13 12:53:00 +0100

    add cmd, entrypoint and pip_install options

--- src/tensorlake/applications/interface/image.py
+++ src/tensorlake/applications/interface/image.py
@@ -14,6 +14,8 @@ class _ImageBuildOperationType(Enum):
     COPY = 2
     ENV = 3
     RUN = 4
+    CMD = 5
+    ENTRYPOINT = 6
 
 
 @dataclass
@@ -24,6 +26,10 @@ class _ImageBuildOperation:
 
 
 class Image:
+    """
+    Represents a Docker image to be built for application functions.
+    """
+
     def __init__(
         self,
         name: str = "default",
@@ -40,15 +46,29 @@ class Image:
 
     @property
     def name(self) -> str:
+        """
+        The name of the image.
+        """
         return self._name
 
     @property
     def tag(self) -> str:
+        """
+        The tag of the image.
+        """
         return self._tag
 
     def add(
         self, src: str, dest: str, options: Dict[str, str] | None = None
     ) -> "Image":
+        """
+        Add a file or directory to the image. Equivalent to the Dockerfile ADD command.
+
+        Args:
+            src (str): The source file or directory path.
+            dest (str): The destination path inside the image.
+            options (Dict[str, str], optional): Additional options for the ADD operation.
+        """
         return self._add_operation(
             _ImageBuildOperation(
                 type=_ImageBuildOperationType.ADD,
@@ -60,6 +80,14 @@ class Image:
     def copy(
         self, src: str, dest: str, options: Dict[str, str] | None = None
     ) -> "Image":
+        """
+        Copy a file or directory to the image. Equivalent to the Dockerfile COPY command.
+
+        Args:
+            src (str): The source file or directory path.
+            dest (str): The destination path inside the image.
+            options (Dict[str, str], optional): Additional options for the COPY operation.
+        """
         return self._add_operation(
             _ImageBuildOperation(
                 type=_ImageBuildOperationType.COPY,
@@ -69,6 +97,14 @@ class Image:
         )
 
     def env(self, key: str, value: str) -> "Image":
+        """
+        Set an environment variable in the image. Equivalent to the Dockerfile ENV command.
+
+        Args:
+            key (str): The environment variable name.
+            value (str): The environment variable value.
+        """
+
         return self._add_operation(
             _ImageBuildOperation(
                 type=_ImageBuildOperationType.ENV, args=[key, value], options={}
@@ -78,12 +114,67 @@ class Image:
     def run(
         self, commands: str | List[str], options: Dict[str, str] | None = None
     ) -> "Image":
+        """
+        Run a command in the image. Equivalent to the Dockerfile RUN command.
+
+        Args:
+            commands (str | List[str]): The command or list of commands to run.
+            options (Dict[str, str], optional): Additional options for the RUN operation.
+        """
         args = commands if isinstance(commands, list) else [commands]
         return self._add_operation(
             _ImageBuildOperation(
                 type=_ImageBuildOperationType.RUN,
                 args=args,
                 options={} if options is None else options,
+            )
+        )
+
+    def pip_install(self, packages: str | List[str]) -> "Image":
+        """
+        Install Python packages using pip in the image. Equivalent to running 'pip install' in a RUN command.
+
+        Args:
+            packages (str | List[str]): The package or list of packages to install.
+        """
+
+        pkgs = packages if isinstance(packages, list) else [packages]
+        install_cmd = f"pip install {' '.join(pkgs)}"
+        return self.run(install_cmd)
+
+    def cmd(self, command: str | List[str]) -> "Image":
+        """
+        Set the default command for the image. Equivalent to the Dockerfile CMD command.
+
+        Setting the CMD in the image is not necessary when using TensorLake, as the application
+        function will override it. However, it can be useful for local testing, or custom images.
+
+        Args:
+            command (str | List[str]): The command or list of commands to set as the default command.
+        """
+
+        args = command if isinstance(command, list) else [command]
+        return self._add_operation(
+            _ImageBuildOperation(
+                type=_ImageBuildOperationType.CMD, args=args, options={}
+            )
+        )
+
+    def entrypoint(self, command: str | List[str]) -> "Image":
+        """
+        Set the entrypoint for the image. Equivalent to the Dockerfile ENTRYPOINT command.
+
+        Setting the ENTRYPOINT in the image is not necessary when using TensorLake, as the application
+        function will override it. However, it can be useful for local testing, or custom images
+
+        Args:
+            command (str | List[str]): The command or list of commands to set as the entrypoint.
+        """
+
+        args = command if isinstance(command, list) else [command]
+        return self._add_operation(
+            _ImageBuildOperation(
+                type=_ImageBuildOperationType.ENTRYPOINT, args=args, options={}
             )
         )
 
